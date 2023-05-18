526258dedd8b45fbf7d1696d6196e91bf99908d6
AksharaMukundSonale <91076766+AksharaMukundSonale@users.noreply.github.com>
2023-05-18 16:08:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,25 +3,21 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
-  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
 }
 
-function modelLoaded()
-{
-  console.log('Model loaded!');
+function modelLoaded() {
+    console.log('Model Loaded!');
 }
 
-function draw()
-{
+function draw() {
   image(video, 0, 0, 300, 300);
-  classifer.classify(video, gotResult);
+  classifier.classify(video, gotResult);
 }
-
 var previous_result = '';
 
-function gotResult(error, results)
-{
-  if(error){
+function gotResult(error, results) {
+  if (error) {
     console.error(error);
   } else {
     if((results[0].confidence > 0.5) && (previous_result != results[0].label)){
@@ -31,9 +27,9 @@ function gotResult(error, results)
       speak_data = 'Object detected is - '+results[0].label;
       var utterThis = new SpeechSynthesisUtterance(speak_data);
       synth.speak(utterThis);
-  
+
       document.getElementById("result_object_name").innerHTML = results[0].label;
-      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(2);
-    }    
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
   }
 }
